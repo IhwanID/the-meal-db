cc16c08ef4f49962f4ccba4f0a0a133279fe19bb
Ihwan <ihwan@indosystem.com>
2019-09-14 09:08:41 +0700

    add search BLOC

---
+++ lib/bloc/search_bloc.dart
@@ -0,0 +1,19 @@
+import '../resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+import '../model/item_model.dart';
+
+class SearchBloc{
+  final _repository = Repository();
+  final _searchFetcher = PublishSubject<ItemModel>();
+
+  Observable<ItemModel> get allMeals => _searchFetcher.stream;
+
+  searchMealsByName(String name) async {
+    ItemModel itemModel = await _repository.searchMeals(name);
+    _searchFetcher.sink.add(itemModel);
+  }
+
+  dispose() {
+    _searchFetcher.close();
+  }
+}
